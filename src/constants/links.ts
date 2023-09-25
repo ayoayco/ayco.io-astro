32666b58c96bda01bbda26fb7b7a1e8bcb2a0413
Ayo <ramon.aycojr@gmail.com>
2023-09-25 22:51:49 +0200

    feat: constants; update deps

---
+++ src/constants/links.ts
@@ -0,0 +1,30 @@
+export type Link = {
+  url: string;
+  icon: string;
+  set?: string;
+  text?: string;
+};
+
+
+const links: Link[] = [
+  {
+    url: "https://ayos.blog",
+    icon: "blog",
+  },
+  {
+    url: "https://ayco.io/@ayo",
+    icon: "mastodon",
+    text: "social"
+  },
+  {
+    url: "https://ayco.io/gh/",
+    icon: "github",
+  },
+  {
+    url: "https://www.npmjs.com/~aayco",
+    icon: "npm",
+    set: "gg"
+  }
+];
+
+export default links;
