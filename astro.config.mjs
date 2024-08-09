6fede8ae6eaf5a14565970cc29841be3943efe6b
Ayo Ayco <ayo@ayco.io>
2024-08-09 17:47:31 +0200

    chore: remove unnecessary config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ayo.ayco.io',
-  integrations: [sitemap(
-    {
-      customPages: ['https://ayo.ayco.io/threads/']
-    }
-  )]
+  integrations: [sitemap()]
 });
