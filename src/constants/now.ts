79750094fac4e6c3da4553f08ff7d1ed23219652
Ayo Ayco <ayo.kilabot+code@gmail.com>
2024-12-25 13:46:56 +0100

    feat: new constant and content for now pages
    - constants/now.ts for he now page; will evolve to be an array for
      previous pages
    - new previous page 2024-09-27
    - new now page content

---
+++ src/constants/now.ts
@@ -0,0 +1,8 @@
+const now = {
+  status: 'Celebrating the year\'s end 🎉',
+  publishDate: '2024-12-25',
+  readableDate: 'the Christmas Day of 2024',
+  publishState: 'while on the way home after a vacation in Germany; now onboard a train from Eindhoven to Amsterdam.'
+}
+
+export default now
