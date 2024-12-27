93193b10de7117bddb26e18b289c74168cfe0419
Ayo Ayco <ayo@ayco.io>
2024-12-27 20:58:26 +0100

    chore: test precommit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,8 +18,6 @@ import { fileURLToPath } from 'node:url';
 
 
 
-
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')
