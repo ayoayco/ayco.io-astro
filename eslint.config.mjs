af25f37d9ca8d697fcd9aef28748d473059ab5e3
Ayo Ayco <ayo@ayco.io>
2024-12-27 16:58:14 +0100

    chore: test format on commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,9 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const gitignorePath = path.resolve(__dirname, '.gitignore')
 
+
+
+
 export default [
   {
     languageOptions: {
