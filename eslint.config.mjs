b158b50fef79f29b490def34c18cfe3349a5329a
Ayo Ayco <ayo@ayco.io>
2024-12-27 17:18:17 +0100

    chore: test lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import astroParser from 'astro-eslint-parser'
 
 import { includeIgnoreFile } from '@eslint/compat'
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
