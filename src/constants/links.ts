39673fb46819fd4474fab2e57c03ac4c3aff757c
Ayo <ayo@ayco.io>
2025-11-15 20:38:08 +0100

    feat: link to masto about

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -57,7 +57,7 @@ export const footerLinks: Link[] = [
   },
   {
     text: 'Mastodon',
-    url: 'https://social.ayco.io/@ayo',
+    url: 'https://social.ayco.io/about',
     icon: 'mastodon',
   },
   {
